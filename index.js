29d4ac82a0758a1287d880b397c2a42b2c63360b
Roshan Vivek Khatri <59066845+roshanvkhatri@users.noreply.github.com>
2021-01-19 19:29:15 +0530

    Updated images path

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var randomNumber2 = Math.floor(Math.random() * 6 + 1);
 
 document
     .querySelector("img.img1")
-    .setAttribute("src", "/images/dice" + randomNumber1 + ".png");
+    .setAttribute("src", "images/dice" + randomNumber1 + ".png");
 
 document
     .querySelector("img.img2")
-    .setAttribute("src", "/images/dice" + randomNumber2 + ".png");
+    .setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
 if (randomNumber1 === randomNumber2) {
     document.querySelector("h1").innerHTML = "Draw";
